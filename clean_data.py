1e407f17fe7aea6f361ebbdc60d4b3970b9668ef
AnhadPahwa <anhadpahwa13@gmail.com>
2026-05-29 15:46:45 +0100

    changed reporting quantity to reporting sales

--- clean_data.py
+++ clean_data.py
@@ -4,6 +4,8 @@ data_files= ["data/daily_sales_data_0.csv", "data/daily_sales_data_1.csv", "data
 df = pandas.concat((pandas.read_csv(filename) for filename in data_files))
 
 df = df[df["product"] == "pink morsel"]
-df = df[["quantity","date","region"]]
+df["price"] = df["price"].str.replace('$', '').astype(float)
+df["sales"] = (df["price"] * df["quantity"]).astype(int)
+df = df[["sales","date","region"]]
 
 data = df.to_csv('data/daily_sales_data_pink.csv', index=False)
