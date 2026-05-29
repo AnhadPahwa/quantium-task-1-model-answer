98a34acda22d5d5b01e37978dec7512116f03297
AnhadPahwa <anhadpahwa13@gmail.com>
2026-05-29 11:01:57 +0100

    cleaned data

---
+++ clean_data.py
@@ -0,0 +1,9 @@
+import csv, pandas
+
+data_files= ["data/daily_sales_data_0.csv", "data/daily_sales_data_1.csv", "data/daily_sales_data_2.csv"]
+df = pandas.concat((pandas.read_csv(filename) for filename in data_files))
+
+df = df[df["product"] == "pink morsel"]
+df = df[["quantity","date","region"]]
+
+data = df.to_csv('data/daily_sales_data_pink.csv', index=False)
